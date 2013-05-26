c5fc7496b22004ceb8a70fbadae98e4c37daaa90
Manju <manjunath.nm89@gmail.com>
2013-05-26 15:36:50 +0530

    moving stuff to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,11 @@ require 'test/unit'
 def assert_false(boolean)
   assert !boolean
 end
+
+def create_roman_util(string)
+  RomanConverter::RomanUtil.new(create_roman_array(string))
+end
+
+def create_roman_array(input_string)
+  input_string.split("")
+end
