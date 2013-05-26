87e3d222dd6881a2deef3958e1a7ae267e79a0b7
Manju <manjunath.nm89@gmail.com>
2013-05-26 19:07:49 +0530

    to_number for string to convert roman numerals to number

--- lib/roman_converter.rb
+++ lib/roman_converter.rb
@@ -1,5 +1,14 @@
-require "roman_converter/version"
+require File.expand_path('../roman_converter/roman.rb', __FILE__)
+require File.expand_path('../roman_converter/roman_util.rb', __FILE__)
+require File.expand_path('../roman_converter/rules.rb', __FILE__)
 
 module RomanConverter
-  # Your code goes here...
+
+  String.class_eval do
+    def to_number
+      roman = RomanConverter::Roman.new(self)
+      roman.convert_to_number
+    end
+  end
+
 end
