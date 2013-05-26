6fa9997f0e999946c84af2aad2ed9061ccd00ad5
Manju <manjunath.nm89@gmail.com>
2013-05-26 15:26:56 +0530

    test cases for repeating succession

--- test/unit/roman_converter/roman_util_test.rb
+++ test/unit/roman_converter/roman_util_test.rb
@@ -5,11 +5,11 @@ require File.expand_path('../../../../test/test_helper.rb', __FILE__)
 class RomanConverter::RomanUtilTest < Test::Unit::TestCase
 
   def test_is_invalid_numerals
-    roman_util = RomanConverter::RomanUtil.new(create_roman_array("XMVIMANJU"))
+    roman_util = RomanConverter::RomanUtil.new(create_roman_array("MXVIMANJU"))
     assert roman_util.is_invalid?
     assert roman_util.is_invalid_numerals?
 
-    roman_util = RomanConverter::RomanUtil.new(create_roman_array("XMVI"))
+    roman_util = RomanConverter::RomanUtil.new(create_roman_array("MXVI"))
     assert_false roman_util.is_invalid_numerals?
 
     roman_util = RomanConverter::RomanUtil.new(create_roman_array("X"))
@@ -17,6 +17,20 @@ class RomanConverter::RomanUtilTest < Test::Unit::TestCase
 
     roman_util = RomanConverter::RomanUtil.new(create_roman_array("AM"))
     assert roman_util.is_invalid_numerals?
+  end
+
+  def test_is_repeating_succession
+    roman_util = RomanConverter::RomanUtil.new(create_roman_array("MXVI"))
+    assert_false roman_util.is_repeating_succession?
+
+    roman_util = RomanConverter::RomanUtil.new(create_roman_array("MMMCM"))
+    assert_false roman_util.is_repeating_succession?
+
+    roman_util = RomanConverter::RomanUtil.new(create_roman_array("MMMMM"))
+    assert roman_util.is_repeating_succession?
+
+    roman_util = RomanConverter::RomanUtil.new(create_roman_array("MMMMCCCCC"))
+    assert roman_util.is_repeating_succession?
   end
 
 private
